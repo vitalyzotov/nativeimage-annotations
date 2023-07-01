ebe49766fd69c1c8660f732c830afdf82a224b82
vzotov <vitalyzotov@gmail.com>
2023-07-01 22:08:25 +0400

    Initial commit

---
+++ src/main/java/ru/vzotov/nativeimage/annotation/ReflectionAccess.java
@@ -0,0 +1,11 @@
+package ru.vzotov.hb.nativeimage;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface ReflectionAccess {
+}
